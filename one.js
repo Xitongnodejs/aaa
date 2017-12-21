128de148abe2c5128723e6ab5b145137e23f6bff
Xitongnodejs <383883149@qq.com>
2017-12-21 20:26:02 +0800

    字符串

---
+++ one.js
@@ -0,0 +1,30 @@
+var hello = "hello node";
+//var xitong = hello.charAt(1);
+//获取字符串中指定字符
+//var xitong = hello.charCodeAt(3);
+//获取指定字符串的unicode编码
+//var xitong = hello.concat(123);
+//字符串拼接
+//var xitong = hello.indexOf('o');
+//查找指定字符的下标
+//var xitong = hello.lastIndexOf('d');
+//从后往前查找指定字符的下标
+//var xitong =  hello.substr(2,5);
+//从原字符串中获取字串。开始的位置，获取的个数
+//var xitong = hello.substring(2,6);
+//从原字符串中获取字串。开始的位置，结束时的位置
+//var xitong = hello.slice(2,5);
+//从原字符串中获取字串。开始的位置，结束时的位置
+//var xitong = hello.toLowerCase();
+//大写转小写
+//var xitong = hello.toUpperCase();
+//小写转大写
+//var xitong = hello.split('e');
+//将字符串以指定字符转换成数组
+//var xitong = hello.replace('e','u');
+//字符串转换
+
+
+var xitong = hello.length;
+console.log(xitong)
+
